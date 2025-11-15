1b63a09e9afbb1a75a5b91c97755fb98a58665a7
Michael Essam <maichelessam2001@gmail.com>
2025-11-15 10:52:29 +0200

    add files

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -48,8 +48,7 @@ const Contact = () => {
       .then(
         () => {
           setLoading(false);
-         // alert("Thank you. I will get back to you as soon as possible.");
-          alert("Will done Niga 👌👍");
+          alert("Thank you. I will get back to you as soon as possible.");
 
           setForm({
             name: "",
