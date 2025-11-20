94551be1f2e2bd5d3ed58dfeefe25316a1f65904
Michael Essam <maichelessam2001@gmail.com>
2025-11-20 09:56:14 +0200

    add files

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -14,20 +14,21 @@ const Ball = (props) => {
   const [decal] = useTexture([props.imgUrl]);
 
   return (
-    <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
-      <ambientLight intensity={0.25} />
-      <directionalLight position={[0, 0, 0.05]} />
-      <mesh castShadow receiveShadow scale={2.75}>
+    <Float speed={1} rotationIntensity={0.5} floatIntensity={1}>
+      <ambientLight intensity={0.4} />
+      <directionalLight position={[0, 0, 0.2]} />
+      <mesh scale={2.2} castShadow receiveShadow>
         <icosahedronGeometry args={[1, 1]} />
         <meshStandardMaterial
-          color='#fff8eb'
+          color="#fff8eb"
           polygonOffset
           polygonOffsetFactor={-5}
           flatShading
         />
+
         <Decal
           position={[0, 0, 1]}
-          rotation={[2 * Math.PI, 0, 6.25]}
+          rotation={[2 * Math.PI, 0, 0]}
           scale={1}
           map={decal}
           flatShading
@@ -40,12 +41,16 @@ const Ball = (props) => {
 const BallCanvas = ({ icon }) => {
   return (
     <Canvas
-      frameloop='demand'
-      dpr={[1, 2]}
+      frameloop="demand"
+      dpr={[1, 1.2]}
       gl={{ preserveDrawingBuffer: true }}
     >
       <Suspense fallback={<CanvasLoader />}>
-        <OrbitControls enableZoom={false} />
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
         <Ball imgUrl={icon} />
       </Suspense>
 
