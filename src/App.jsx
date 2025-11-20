5436df86dd5b6fe94fd2c28b7b8a94c5e8faff7f
Michael Essam <maichelessam2001@gmail.com>
2025-11-20 09:19:50 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -16,10 +16,10 @@ function App() {
           <Hero />
         </div>
         <About />
-        <Experience />
+        {/* <Experience /> */}
         <Tech />
         <Works />
-        <Feedbacks />
+        {/* <Feedbacks /> */}
         <div className='relative z-0'>
         <Contact />
         <StarsCanvas />
